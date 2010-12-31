c22f265f5ef912660ef3cbb60a3a73cdc875b0ee
Hudson <hudson@ubuntu>
2010-12-31 04:38:38 +0100

    Aanpassing aan de ear pom.

--- ZephyrBank-web/src/main/java/net/byonder/zephyrbank/bean/BoekingenBean.java
+++ ZephyrBank-web/src/main/java/net/byonder/zephyrbank/bean/BoekingenBean.java
@@ -4,53 +4,52 @@ import javax.ejb.EJB;
 import javax.inject.Named;
 
 import net.byonder.zephyrbank.exceptions.OnvoldoendeSaldoExceptie;
-import net.byonder.zephyrbank.model.Rekening;
 import net.byonder.zephyrbank.service.RekeningService;
 import net.byonder.zephyrbank.util.FacesUtil;
 
-
 /**
  * @author jvdgriendt
- *
+ * 
  */
-@Named(value="boekingenBean")
+@Named(value = "boekingenBean")
 public class BoekingenBean {
 
-	
 	@EJB
 	RekeningService rekeningService;
-	
-	private Rekening rekeningVan;
-	
-	private Rekening rekeningNaar;
-	
+
+	private long rekeningVan;
+
+	private long rekeningNaar;
+
 	private float bedrag;
-	
+
 	/**
 	 * @return the rekeningVan
 	 */
-	public Rekening getRekeningVan() {
+	public long getRekeningVan() {
 		return rekeningVan;
 	}
 
 	/**
-	 * @param rekeningVan the rekeningVan to set
+	 * @param rekeningVan
+	 *            the rekeningVan to set
 	 */
-	public void setRekeningVan(Rekening rekeningVan) {
+	public void setRekeningVan(long rekeningVan) {
 		this.rekeningVan = rekeningVan;
 	}
 
 	/**
 	 * @return the rekeningNaar
 	 */
-	public Rekening getRekeningNaar() {
+	public long getRekeningNaar() {
 		return rekeningNaar;
 	}
 
 	/**
-	 * @param rekeningNaar the rekeningNaar to set
+	 * @param rekeningNaar
+	 *            the rekeningNaar to set
 	 */
-	public void setRekeningNaar(Rekening rekeningNaar) {
+	public void setRekeningNaar(long rekeningNaar) {
 		this.rekeningNaar = rekeningNaar;
 	}
 
@@ -62,38 +61,39 @@ public class BoekingenBean {
 	}
 
 	/**
-	 * @param bedrag the bedrag to set
+	 * @param bedrag
+	 *            the bedrag to set
 	 */
 	public void setBedrag(float bedrag) {
 		this.bedrag = bedrag;
 	}
 
-	public void opnemen(){
+	public void opnemen() {
 		float bedragNegatief = bedrag * (-1);
 		try {
-			rekeningVan.muteerSaldo(bedragNegatief);
-			FacesUtil.addInfoMessage("Bedrag is opgenomen van de rekening" );
+			rekeningService.haalRegekeningOpViaId(rekeningVan).muteerSaldo(bedragNegatief);
+			FacesUtil.addInfoMessage("Bedrag is opgenomen van de rekening");
 		} catch (OnvoldoendeSaldoExceptie e) {
 			FacesUtil.addWarningMessage("Rekening heeft onvoldoende saldo om de mutatie uit te voeren.");
 		}
 	}
-	
-	public void storten(){
+
+	public void storten() {
 		try {
-			rekeningVan.muteerSaldo(bedrag);
-			FacesUtil.addInfoMessage("Bedrag is opgenomen van de rekening" );
+			rekeningService.haalRegekeningOpViaId(rekeningVan).muteerSaldo(bedrag);
+			FacesUtil.addInfoMessage("Bedrag is opgenomen van de rekening");
 		} catch (OnvoldoendeSaldoExceptie e) {
 			FacesUtil.addWarningMessage("Rekening heeft onvoldoende saldo om de mutatie uit te voeren.");
 		}
 	}
-	
-	public void overboeken(){
+
+	public void overboeken() {
 		try {
-			rekeningService.overboeking(bedrag, rekeningVan, rekeningNaar);
-			FacesUtil.addInfoMessage("Bedrag is overgeboekt." );
+			rekeningService.overboeking(bedrag, rekeningService.haalRegekeningOpViaId(rekeningVan), rekeningService.haalRegekeningOpViaId(rekeningNaar));
+			FacesUtil.addInfoMessage("Bedrag is overgeboekt.");
 		} catch (OnvoldoendeSaldoExceptie e) {
 			FacesUtil.addWarningMessage("Rekening heeft onvoldoende saldo om de mutatie uit te voeren.");
 		}
 	}
-	
+
 }
